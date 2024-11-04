93c47d6048ed17c7a5faa71cace8e44e25bcbeb5
QuantumDrifter22 <Michaeldiecks22@gmail.com>
2024-11-04 09:31:44 -0600

    Delete frontpage directory

--- frontpage/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Vote(models.Model):
-    ip_address = models.CharField(max_length=100)
-    candidate = models.CharField(max_length=100)
-    voted_at = models.DateTimeField(auto_now_add=True)
-
-def __str__(self):
-    return f"IP {self.ip_address} voted for {self.candidate}"
